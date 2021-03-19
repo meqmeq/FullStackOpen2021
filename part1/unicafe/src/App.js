8dec17f8e7fe146bf48848c1ea29ee637197bee4
Marc <mecquilatan@gmail.com>
2021-03-19 13:02:49 -0400

    Finished unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+const Button = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => props.setF(props.value + 1)}>{props.text}</button>
   );
-}
+};
+
+const Statistics = (props) => {
+  // let average = (props.good - props.bad) / props.total;
+  // let positive = props.good / props.total;
+  return (
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  let total = good + bad + neutral;
+  let average = (good - bad) / total;
+  let positive = (good / total) * 100;
+  return (
+    <>
+      <div>
+        <h1>Give Feedback</h1>
+        <Button setF={setGood} value={good} text="good" />
+        <Button setF={setNeutral} value={neutral} text="neutral" />
+        <Button setF={setBad} value={bad} text="bad" />
+
+        <h1>Statistics</h1>
+        {total ? (
+          <>
+            <table>
+              <tbody>
+                <Statistics text="good" value={good} />
+                <Statistics text="neutral" value={neutral} />
+                <Statistics text="bad" value={bad} />
+                <Statistics text="all" value={total} />
+                <Statistics text="average" value={average} />
+                <Statistics text="positive" value={positive + " %"} />
+              </tbody>
+            </table>
+          </>
+        ) : (
+          <p>No feedback given</p>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default App;
